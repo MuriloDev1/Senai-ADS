bb80db3c20f63f4fad012dec2b160b1aad012298
MuriloDev1 <muriloantunesdasilva264@gmail.com>
2025-04-11 20:21:03 -0300

    Realização das atividades 8, 9 e 15

--- semestre-1/Linguagem-de-Programacao/LP_Lista_03/exercicio-08/src/Main.java
+++ semestre-1/Linguagem-de-Programacao/LP_Lista_03/exercicio-08/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int num;
+
+        System.out.println("Insira um número inteiro: ");
+        num = scanner.nextInt();
+        boolean isOdd = num % 2 == 0;
+
+            System.out.println(isOdd ? "Par" : "ímpar");
     }
 }
--- semestre-1/Linguagem-de-Programacao/LP_Lista_03/exercicio-09/src/Main.java
+++ semestre-1/Linguagem-de-Programacao/LP_Lista_03/exercicio-09/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c;
+
+        System.out.println("Insira três valores *separados por um espaço");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+
+        if(a<(b+c) && a>Math.abs(b-c) && b<(a+c) && b>Math.abs(a-c) && c<(a+b) && c>Math.abs(a-b)) {
+            System.out.println("A, B e C formam um triângulo");
+            if (a==b && a==c)
+                System.out.println("Triângulo Equilátero");
+            else if (a==b || b==c || a==c)
+                System.out.println("Triângulo Isósceles");
+            else
+                System.out.println("Triângulo Escaleno");
+        } else
+            System.out.println("A, B e C não formam um triângulo");
     }
 }
